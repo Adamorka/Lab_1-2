1a8e022c3e9523dd310acded7c69d706a40efb71
Adamorka <sashenka12390@gmail.com>
2022-06-14 20:12:06 +0300

    first commit

---
+++ shapes/Shape.java
@@ -0,0 +1,21 @@
+package shapes;
+
+import java.awt.*;
+
+public abstract class Shape {
+    private Color borderColor;
+
+    public Shape(){
+        borderColor = Color.BLACK;
+    }
+
+    public Color getBorderColor(){
+        return borderColor;
+    }
+
+    public abstract void refreshShape(shapes.Point point);
+
+    public abstract void draw(Graphics g);
+}
+
+
